2bf9862eb35e7661c03b8c2b3c64af636bb0312c
bigDre82 <andre.oscco.14@gmail.com>
2023-06-26 20:02:00 +0200

    funcionalidades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
-}
+  result: string = '0';
+
+  handleButtonClick(value: number | string): void {
+    value = value.toString();
+
+    if (this.result === '0') {
+      this.result = value;
+    } else {
+      this.result += value;
+    }
+  }
+
+  resetResult(): void {
+    this.result = '0';
+  }
+
+  calculateResult(): void {
+    try {
+      this.result = eval(this.result);
+    } catch (error) {
+      this.result = 'Error';
+    }
+  }
+  }
